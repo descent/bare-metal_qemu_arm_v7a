7df40e285e8c6e0eceb7b1c5f8a8c7d3c0d564da
descent <descent0@gmail.com>
2017-05-04 13:37:36 +0800

    show different message for VEXPRESS-A9 and ARM926EJ-S

--- test.c
+++ test.c
@@ -19,10 +19,42 @@ void print_uart0(const char *s)
     s++; /* Next char */
   }
 }
- 
+
+  volatile unsigned int * const ptr = (unsigned int *)0x70000000;
+#if 0
 void c_entry() 
 {
-  //print_uart0("Hello world!\n");
-  *UART0DR = 'a';
-  *UART0DR = 'b';
+  *ptr = 'b';
+  #if 0
+  char s[5];
+
+  s[0]='a';
+  while(1);
+  #endif
 }
+#endif 
+#if 1
+void c_entry() 
+{
+#ifdef VEXPRESS_A9
+  const char str[]="hello VEXPRESS_A9";
+#else
+  char str[]="hello ARM926EJ-S";
+#endif
+  print_uart0("Hello world!\r\n");
+  print_uart0(str);
+
+#if 0
+  const char *s;
+  //s=(const char *)0x4;
+  s = str;
+  while(*s != '\0') 
+  { /* Loop until end of string */
+    *UART0DR = (unsigned int)(*s); /* Transmit char */
+    s++; /* Next char */
+  }
+  //*UART0DR = 'a';
+  //*UART0DR = 'b';
+#endif
+}
+#endif
